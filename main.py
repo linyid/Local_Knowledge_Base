00bf455fa0b90461086605987448e4c956fe852d
linyid <yi.lin2@mail.mcgill.ca>
2025-07-28 21:39:35 -0400

    small updates to project

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 from ingest import load_documents
-from embed import embed_documents, save_vectorstore, load_vectorstore
-from retrieve import retrieve_docs
-from chat import ask_question
 import json
 
 
 def main():
-    # Load base directories from config.json
-    with open("config.json", "r") as f:
-        config = json.load(f)
-        base_dirs = config.get("include_dirs", [])
+    collected_docs = load_documents()
+    print(collected_docs)
+    print(f"Collected {len(collected_docs)} documents.")
 
-    docs = load_documents(base_dirs)
-    vectordb = embed_documents(docs)
-    save_vectorstore(vectordb)
 
-    while True:
-        query = input("\nAsk a question (or type 'exit'): ")
-        if query.lower() == 'exit':
-            break
-        top_docs = retrieve_docs(query, vectordb)
-        response = ask_question(query, top_docs)
-        print("\nAnswer:\n", response)
 
 
 if __name__ == "__main__":
     main()
+    
